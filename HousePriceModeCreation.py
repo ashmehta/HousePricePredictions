005663a206674b50089fbaa13789aa803490f800
ashmehta <mehtaakshit7@gmail.com>
2025-04-02 15:58:56 -0700

    Compared Models and found the best one

--- HousePriceModeCreation.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, mean_absolute_error,r2_score
-from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import make_pipeline
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import GridSearchCV
